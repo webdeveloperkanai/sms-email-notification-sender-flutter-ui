15f2d44e5ec390d6e7e519ab35e695a2ef23dcf9
Kanai Shil <70555095+webdeveloperkanai@users.noreply.github.com>
2022-07-12 10:35:13 +0530

    Update config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -4,6 +4,6 @@ import 'package:flutter/material.dart';
 
 const APP_NAME = "SERVICES - DEV SEC IT";
 const APP_VER = "1.4.3";
-const APP_SERVER = "https://dev.devsecit.com/services/call.php";
+const APP_SERVER = "";
 
 const APP_COLOR = Color.fromARGB(255, 97, 4, 105);
